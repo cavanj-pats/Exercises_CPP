b85ebc9b12fe5d6ffd529677017b95879fea2856
cavanj-pats <cavanj@gmail.com>
2021-10-06 20:56:27 -0400

    LeetCodePhoneNumber effort

--- LeetCodePhoneNumCharMap.cpp
+++ LeetCodePhoneNumCharMap.cpp
@@ -7,10 +7,12 @@
 
 using namespace std;
 
+/*
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
         int * inputDigits = new int[digits.length()-1];
+        int currDigit =0;
         mapsize = 1;
         
         for(int i = 0; i<digits.length(); i++){       
@@ -18,15 +20,46 @@ public:
             mapsize *= map[inputDigits[i]].length();
         }
         
+        for(int i=0; i<digits.length(); i++){
+            //if there is no input digit can return
+        
+            for (int j=0; j<map[inputDigits[i]].length(); j++){
+                //if the digit refers to a map with no characters can return here
+                if (inputDigits[i]==0 || inputDigits[i]=1){
+                    //exit
+                }
+                
+
+
+
+            }
+        }
+
+        switch (digits.length()){
+            case 0:
+                return "";
+            case 1:
+
+            case 2:
+
+            case 3:
+
+            case 4:
+
+            default:
+                return ""
+        }
+
         //while loop to traverse each inputDigits
-        do {
-            for (int j=0; j<mapsize; j++){
+        for (int j=0; j<mapsize; j++){
                 //populate the vector
+                record = "";
+                letterCombinations.pushback(traverse(0, digits, &record));
 
             }
 
 
-        }
+        
 
         delete [] inputDigits;
     }
@@ -34,12 +67,14 @@ public:
 private:    
     string map[10];
     int mapsize ;
+    string record;
+
 
 };
 
 Solution::Solution(){
-    map[0] = "";
-    map[1] = "";
+    map[0] = "0";
+    map[1] = "1";
     map[2] = "abc";
     map[3] = "def";
     map[4] = "ghi";
@@ -48,6 +83,19 @@ Solution::Solution(){
     map[7] = "pqrs";
     map[8] = "tuv";
     map[9] = "wxyz";
+}
+
+string oneDigit(int digit){
+    string temp;
+    temp.push_back(map[digit][0]);
+    temp.push_back(",");
+    temp.push_back(map[digit][1]);
+    temp.push_back(",");
+    temp.push_back(map[digit][2");
+    temp.push_back(",");
+    if(digit == 7 || digit ==8) temp.push_back(map[digit][3]);
+
+    return temp;
 }
 
 void merge(string &map, &inputDigits int num){
@@ -65,6 +113,36 @@ void merge(string &map, &inputDigits int num){
         
 }
 
+push  character 1,  character 1, character 1 character 1
+push  character 1, character 1 character 1, character 2
+1 1 1 1
+1 1 1 2
+1 1 1 3
+1 1 1 4
+1 1 2 1
+1 1 2 2
+1 1 2 3
+1 1 2 4
+1 1 3 1 
+1 1 3 2
+1 1 3 3 
+1 1 3 4
+1 2 1 1
+1 2 1 2         
+...
+//need to solve for up to four digits
+string traverse(int inputDigit, string digits, string *record){
+    if inputdigit < digits.length(){} 
+        inputDigit++;
+        traverse = ;
+}
+
+
+
+
+*/
+
+
 
 int main(){ 
    string map[10];
@@ -79,12 +157,13 @@ int main(){
     map[8] = "tuv";
     map[9] = "wxyz";
 
-   int mapsize = 1;
+   int mapsize = 1;  //this will be the size of the array that holds the solution
+   //max is four digits.  max size could be 
    
-   Solution mySol;
+  // Solution mySol;
 
 
-   string strDigits = "78";
+   string strDigits = "6789";
    int * inputDigits = new int[strDigits.length()-1];
    
    if(strDigits.length() ==0) mapsize =0;
@@ -102,10 +181,29 @@ int main(){
 
     //cout << ( map[3])[2] <<"\t" << (map[3])[3]<< endl;
 
-    for(int i=0; i<strDigits.length();i++)
-        cout << map[inputDigits[i]] << endl;
+    for(int i=0; i<strDigits.length();i++){
+        cout << map[inputDigits[i]] <<"\t"<<map[inputDigits[i]].length() << endl;
+        cout << map[inputDigits[i]][1] << endl;
+    }
 
-
+     string temp;
+     temp.push_back('"');
+    temp.push_back(map[7][0]);
+    temp.push_back(map[8][0]);
+    temp.push_back('"');
+    temp.push_back(',');
+    temp.push_back('"');
+    temp.push_back(map[7][1]);
+    temp.push_back('"');
+    temp.push_back(',');
+    temp.push_back('"');
+    temp.push_back(map[7][2]);
+    temp.push_back('"');
+    temp.push_back(',');
+    temp.push_back('"');
+    temp.push_back(map[7][3]);
+    temp.push_back('"');
+    cout << temp << endl;
 
     delete[] inputDigits;
     delete[] mergedMap;
