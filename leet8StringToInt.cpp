fe5c89b922d753fee3a44b2ca8f8f716f9d9af23
cavanj-pats <cavanj@gmail.com>
2021-10-26 23:11:41 -0400

    leet8 String to Int accepted!

--- leet8StringToInt.cpp
+++ leet8StringToInt.cpp
@@ -18,11 +18,11 @@ whitespace  sign  number   text
 
 using namespace std;
 
-int number(string, int);
+long long int number(string, int);
 int sign (string, int);
 
 int whitespace(string s, int start){
-    for( int i=start; i<s.length()-1; i++)
+    for( int i=start; i<s.length(); i++)
         if(s[i]!=' ')
             return sign(s, i);
 
@@ -31,32 +31,64 @@ int whitespace(string s, int start){
 }
 
 int sign(string s, int start){
-    if(s[start]=='-') return -1* number(s, start+1);
-    if(s[start]=='+') return number(s, start+1);
-    return number(s,start);
+    long long int temp=0;
+    if(s[start]=='-')temp = -1* number(s, start+1);
+    
+    if(s[start]=='+') {
+        temp =  number(s, start+1);
+    }
+    else{
+      if(temp==0) temp = number(s,start);  
+    }
+    
+    if(temp <= 1*pow(-2,31)) return 1*pow(-2,31);
+    if(temp>=pow(2,31)-1){
+        return 1*pow(2,31)-1;
+    }
+    else{
+        return temp;
+    }
+    
 }
 
-int number(string s, int start){
+long long int number(string s, int start){
     //if not a number return 0
     //if leading zero ignore
     int end;
     int x=0;
-    int temp=0;
+    long long int temp=0;
     bool val = false;
     bool stop = false;
     string num="";
+    switch(s[start]-48){
+        case 0:case 1: case 2: case 3: case 4: case 5: case 6:
+        case 7: case 8: case 9:
+            break;
+
+        default:
+            return 0;
+    }
     for(int i=start; i<s.length(); i++){
         if(s[i]-48 >0 && !val) val=true;
-        if(s[i]-48 >= 0 && s[i]-48 <=9){
+
+        if( (s[i]-48 >= 0 && s[i]-48 <=9)){
             if(val && !stop) num = num + s[i]; //if i don't use {} it wont go to else
+            if(i==s.length()-1 && !stop){
+                //this is invoked if reached end of string
+                end = i;
+                stop = true;
+            }
+
         }
         else{
+            //this is invoked if the string has non-numeric characters after numbers
             if(!stop) end = i-1; 
             stop = true;
              
         }
 
     }
+    if(num.length()>10) return pow(2,31);
 
     for(int j=end; j>=start; j--){
         temp+=(s[j]-48)*pow(10,x);
@@ -78,8 +110,8 @@ public:
 int main(){
     Solution sol;
 
-    //int x = sol.myAtoi("   0002147483649");
-    int x = sol.myAtoi("     +987654 Peckerhead");
+   // int x = sol.myAtoi("   0002147483648");
+    int x = sol.myAtoi("2147483646");
     cout << x << endl;
 
     return 0;
