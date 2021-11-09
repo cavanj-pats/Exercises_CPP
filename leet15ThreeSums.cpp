75b701c6a6de83048fde67e4a8ee850b073e51d6
cavanj-pats <cavanj@gmail.com>
2021-11-08 21:04:34 -0500

    leet 15 3Sum working and accepted

--- leet15ThreeSums.cpp
+++ leet15ThreeSums.cpp
@@ -2,7 +2,9 @@
 
 #include <iostream>
 #include <vector>
+#include <bits/stdc++.h> //needed to use sort function.
 
+///need a case for all zeros;
 
 using namespace std;
 
@@ -10,65 +12,106 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
             int i=0;
+            int j=nums.size()-1;
+
             int z=0;
             int k=0;
-            bool temp1=false;
+            bool allZeroes=true;
             bool temp2=false;
 
             vector<vector<int>> a;
             vector<int> two;
             vector<int> temp;
-
+            
+            if (nums.empty()) return a;
+            if (nums.size()==1 && nums[0] == 0) return a;
+            if (nums.size()<3) return a;
+            
             for(auto num: nums){
-                for(int j=0; j<nums.size(); j++){
-                    if(j!=i ) temp.push_back(nums[j]);
-                    
+                if(num != 0) allZeroes = false;
+                if(!allZeroes) break;
+            }    
+            
+            if (nums.size() == 3) {
+                if (allZeroes){
+                    temp = {0,0,0};
+                    a.push_back(temp);
+                    return a;
                 }
-                two = twoSum(temp, -1*num);
-                if(two.size()!=0){
-                    two.push_back(num);  //i think it is ok that it is in the back
-                    a.push_back(two);
-                    
-                }
+                else
+                    if(nums[0]+nums[1]+nums[2] == 0){
+                        temp.push_back(nums[0]);
+                        temp.push_back(nums[1]);
+                        temp.push_back(nums[2]);
+                        a.push_back(temp);
+                        return a;
+                    }
+                else
+                    return a;
 
-                temp.clear();
-                i++;
+            }
+            if (allZeroes){
+                temp = {0,0,0};
+                a.push_back(temp);
+                return a;
             }
 
-        return a;
-         
-    }
+            if(nums.size()%2 == 0)
+                z=nums.size()/2;
+            else
+                z=nums.size()/2+1;
 
-private:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> idx; //this is a vector of the only possible indexes in nums that might be a solution
-        vector<int> ans;
-        bool solutionFound = false;
-       /*
-        for(int i=0; i<nums.size(); i++){
-            //if(nums[i]<= target)  cannot use for negative numbers
-                idx.push_back(i);
-        }
-        */
+            
 
-        ans.clear();
+            sort(nums.begin(), nums.end());
+            //
+            while (k < nums.size()-2 ){
+                if(k==0 || (k>0 && nums[k]!=nums[k-1])){
+                    i=k+1; j=nums.size()-1;
+                    while (i<j){
+                        temp.clear();
+                        if(nums[i] + nums[j] == -1*nums[k]){
+                            temp.push_back(nums[k]);
+                            temp.push_back(nums[i]);
+                            temp.push_back(nums[j]);
+                            a.push_back(temp);
 
-       
-        for(int i=0; i<nums.size(); i++){
-            for(int j=0; j<nums.size(); j++)
-                if((j!=i && nums[i]+ nums[j] == target) && !solutionFound){
-                    ans.push_back(nums[i]);
-                    ans.push_back(nums[j]);
-                    solutionFound = true;
+                            //avoid duplicate solution (sorted vector)
+                            while (i <j && nums[i] == nums[i+1])
+                                i++;
+                            //avoid duplicate solution
+                            while (i<j && nums[j-1] ==  nums[j]) 
+                                j--;
+                            
+                            i++; j--;  //moves to next index that has not already been checked
+                        }
+                        else if(nums[i] + nums[j] < -1*nums[k]){
+                            i++;
+                        }
+                        else{
+                            j--;
+                        }
+                    }
                 }
-        }
-        return ans;
+                k++;
+            }        
+              
+        return a;
+   
     }
+
+
 };
 
 
 int main(){
-    vector <int> a = {2,0,-1,1,5,-1};
+    //vector <int> a = {2,0,-1,1,5,-1};
+   // vector <int> a = {-1,0,1,2,-1,-4};
+  // vector <int> a = {-1, 0, 1};
+    //vector <int> a = {0,0,0,0};
+    //vector <int> a = {-2,0,1,1,2};
+    //vector <int> a = {-1,0,1,2,-1,-4,-2,-3,3,0,4};
+    vector <int> a = {0,-4,-1,-4,-2,-3,2};
     vector<vector<int>> ans;
     Solution sol;
     ans = sol.threeSum(a);
