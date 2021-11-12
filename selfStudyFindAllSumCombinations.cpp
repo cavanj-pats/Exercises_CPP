09a5cddf036148edea9f820b20866c6caa42baf1
cavanj-pats <cavanj@gmail.com>
2021-11-11 20:32:17 -0500

    Self Study Find All Sums working recursion

---
+++ selfStudyFindAllSumCombinations.cpp
@@ -0,0 +1,87 @@
+// https://www.educative.io/m/find-all-sum-combinations
+//
+//Find All Sum Combinations
+//selfStudyFindAllSumCombinations.cpp  11/11/2021
+
+/*
+1. Hint was use recursion and two lists
+
+if running sum == target then make sure to return solution
+if running sum is < target add next number
+if running sum > target then we don't need to look any more
+
+use of duplicate numbers is accepted   i.e. nums {2, 3, 5,8} target =8  one sol'n [2,2,2,2]
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+vector<vector<int>> allSumCombos(vector<int>, int);
+void combineAll_rec(vector<int>&, int , int , int , vector<vector<int>>& ,vector<int>& );
+
+
+vector<vector<int>> allSumCombos(vector<int> nums, int target){
+    vector<vector<int>> output;
+    vector<int> result;
+    
+    combineAll_rec(nums, target, 0,0,output,result);
+    return output;
+};
+
+
+
+ void combineAll_rec(vector<int> &nums, int target, int start, int runningSum, 
+        vector<vector<int>>& output,vector<int>& result    ){    
+    
+    int currSum=runningSum;
+
+    if (runningSum == target){
+        output.push_back(result);
+    }
+    else if (runningSum > target){
+        return;   //never gets here
+    }
+    else {
+        for (int i = start; i<nums.size(); ++i){
+            currSum =currSum + nums[i];
+            if (currSum <= target){
+                result.push_back(nums[i]);
+                combineAll_rec(nums, target,i,currSum,output,result);
+                result.pop_back();
+                currSum = runningSum;
+
+            }
+            else{
+                return;
+            }
+        }
+    }
+}
+
+
+
+int main(){
+    vector<int> nums = {1,2,3,5,8};
+    int target = 8;
+    vector<vector<int>> ans = allSumCombos(nums, target);
+
+    
+    for (auto a: ans){
+        cout << "[" ;
+        for(int i=0; i<a.size(); ++i){
+            if(i == a.size()-1){ 
+                cout << a[i];
+            }
+            else{
+                cout << a[i]<< ", " ;
+            }
+        }
+        cout<< "]" << endl;
+    }
+
+    return 0;
+}
