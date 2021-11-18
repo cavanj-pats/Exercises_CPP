994c64d22a19483ed91f4d97c2e836827c1cc1b1
cavanj-pats <cavanj@gmail.com>
2021-11-18 17:19:34 -0500

    leet61 working accepted

--- leet61 Rotate List.cpp
+++ leet61 Rotate List.cpp
@@ -15,43 +15,101 @@ using namespace std;
       ListNode(int x, ListNode *next) : val(x), next(next) {}
   };
 
-void oneRight(ListNode*, ListNode*, ListNode*);
+int NODECOUNT = 0;
+
+void oneRight(ListNode*, ListNode*, ListNode*, int , int);
 
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
         ListNode* temp = head;
         ListNode* newHead = new ListNode;
+        int counter=0;
         if (head == nullptr || head->next == nullptr) return head;
         if (k <= 0) return head;
-        while (k>0){
-            oneRight(head, temp, newHead);
-            k--;
-            if (k==0) break;
-            temp = newHead;
-            head = newHead;
-            newHead = new ListNode;
+        
+        //oneRight(head, temp, newHead, counter, k);
+         //counter = 0;
+        while (temp->next !=nullptr){
+            counter++;
             
-    }
+            temp = temp->next;
+        }
+
+        
+        NODECOUNT = counter+1;  //length of list
+        if (k == NODECOUNT) return head;
+
+        if (k>NODECOUNT)  k=k%NODECOUNT;  //i think this will recalculate k in terms of list position for rotation
+        //the below line is a little dangerous is it could create a circular list.
+        temp->next = head;   //since we are swapping at least one node this has to point to the old head
+        temp = head;
+        for (int i=1; i<NODECOUNT -k; ++i){
+            temp = temp->next;
+
+        }
+        //should be at teh point of rotation
+        newHead = temp->next;
+        temp->next = nullptr;
 
         return newHead;
 
     }
 };
 
+//the below code woudl not work. 
+//each run of the recursion renews the snapshot of head and temp and newHead
+//i did get one rotation working but I think that was when the node assignments and swapping were in teh while loop.
+void oneRight(ListNode* head, ListNode* temp, ListNode* newHead, int counter, int k){
+   counter = 0;
+   while (temp->next !=nullptr){
+       counter++;
+       temp = temp->next;
+   }
+   temp->next = head;   //since we are swapping at least one node this has to point to the old head
 
-void oneRight(ListNode* head, ListNode* temp, ListNode* newHead){
-       
+   NODECOUNT = counter+1;  //length of list
+    
+    if (k>NODECOUNT)  k=k%NODECOUNT;  //i think this will recalculate k in terms of list position for rotation
+    
+    temp = head;
+    for (int i=0; i<NODECOUNT-k; ++i){
+        temp = temp->next;
+
+    }
+     //should be at teh point of rotation
+     newHead = temp->next;
+     temp->next = nullptr;
+     //head = newHead;   //funny head returns to nullptr in calling function
+
+    return ;
+
+
+   
+   
+    /*   
     if (temp->next->next == nullptr ){
-        newHead->val = temp->next->val;
-        newHead->next = head;
-        temp->next = nullptr;
-        return;      
+       NODECOUNT = counter + 2;
+       
+       newHead = head; 
+       return;
+         
     }     
            
-    oneRight(head, temp->next, newHead);
+    oneRight(head, temp->next, newHead, counter+1, k);
+    
+
+    if ((NODECOUNT-counter+1) >= k){
+       // ListNode* t = new ListNode;
+        newHead->val = temp->next->next->val;
+        newHead->next = head;
+        temp->next = nullptr;
+        head = newHead;
+        //return;    
+    }
                  
     return;
+    */
 
 }
 
@@ -79,7 +137,7 @@ int main(){
 
     cout << endl<< "After two rotations: " << endl;
 
-    temp = sol.rotateRight(head, 3);
+    temp = sol.rotateRight(head, 5);
 
     while (temp != nullptr){
         cout << temp->val << ", " ;
