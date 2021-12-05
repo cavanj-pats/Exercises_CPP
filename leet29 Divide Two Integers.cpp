7d62135a3926bd520c616a6d93b97275fc7f6ac5
cavanj-pats <cavanj@gmail.com>
2021-12-04 21:07:03 -0500

    leet29 Divide Two Int using Bit Shift

--- leet29 Divide Two Integers.cpp
+++ leet29 Divide Two Integers.cpp
@@ -8,73 +8,48 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
 
 
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-      // int numerator;
-       //int denominator;
-        int ans;
+        long long numerator = (long long) dividend;
+        long long denominator = (long long) divisor;
+        
         long long counter=0;
         bool isNeg=false;
-
-        
+                
        // if( (long long) dividend == pow(-2,31) && divisor <0) return pow(2,31)-1;
-        if (dividend ==0) return 0;
-        if (divisor == 1) return dividend;
-        if (divisor == -1 && dividend == pow(-2,31)) return pow(2,31)-1;
-        if (divisor == -1 ) return -1 * dividend;
+        if (numerator ==0) return 0;
+        if (denominator == 1) return (int) numerator;
+        if (denominator == -1 && numerator == pow(-2,31)) return pow(2,31)-1;
+        if (denominator == -1 ) return (int) -1 * numerator;
        
-
-        if (dividend < 0 ){
-            if (divisor < 0){
-                //result is positive
-               //do nothing
-            }
-            else{
-                //dividend =-1 * dividend;
-                isNeg = true;
-            }
+        if ( numerator < 0 ){
+            numerator = -1*numerator ; //convert to positiv
+            isNeg = !isNeg;
+            
         }
-        else{
-            if(divisor < 0){
-                //divisor = -1 * divisor;
-                isNeg = true;
-            }
+        if(denominator < 0){
+            denominator = -1 * denominator;
+            isNeg = !isNeg;
         }
 
-        if (dividend < 0 && divisor < 0) {
-              while (dividend <= divisor){
-                  dividend -= divisor;
-                  ++counter;
-              }
-
-               
-        } else if(dividend > 0 && divisor >0) {
-              while (dividend >= divisor){
-                  dividend -= divisor;
-                  ++counter;
-              }
-        } else if (dividend > 0 && divisor <0){
-                while (dividend >= -1*(long long) divisor){
-                  dividend += divisor;
-                  ++counter;
-              }
-
-        } else        {
-                while ( dividend <= -1*(long long) divisor){
-                  dividend += divisor;
-                  ++counter;
-                }
-           
-                      
+        for (int n = 31; n > -1; --n){
+            //have to cast otherwise result will be zero if divisor << n exceeds 32 bits
+            //since dividend is an int a large divisor<<n won't run through the 'if'
+            if (( denominator<<n ) <= numerator){
+                numerator = numerator - (denominator << n) ;
+                counter += 1<<n;
+                
+                if (!isNeg && counter >= pow(2,31)-1) return pow(2,31)-1;
+                if (isNeg && counter >= pow(2,31)) return pow(-2,31);
+            }
         }
 
         if(isNeg) counter = counter *-1;
-        if (counter > pow(2,31)-1) return pow(2,31)-1;
-        if (counter < pow(-2,31)) return pow(-2,31);
-       
+               
         return (int) counter;
     }
 };
@@ -82,8 +57,10 @@ public:
 
 int main(){
     Solution sol;
-
-    std::cout << "5/2 =" << sol.divide (5,2);
+    //std::cout << "2 << 31 " << ((long long) 2<<31) << endl;
+   // std::cout << "7/-3 =" << sol.divide (7,-3) << endl;
+    //std::cout << "-2147483648/2 =" << sol.divide(-2147483648,2) << endl;
+    std::cout << "2147483647/2 =" << sol.divide(2147483647,2) << endl;
     return 0;
     
 }
