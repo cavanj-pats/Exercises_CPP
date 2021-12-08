5b833c284d33fbcee07a0c7c3cf4f399970f56ed
cavanj-pats <cavanj@gmail.com>
2021-12-07 21:08:54 -0500

    leet 466 used _geeky_coder_ code accepted

--- leet466HammingDistanceSum.cpp
+++ leet466HammingDistanceSum.cpp
@@ -1,5 +1,15 @@
 //leet 466 hamming distance sum
 
+/*
+For each bit position 1-32 in a 32-bit integer belonging to the array, we count the number of integers 
+in the array which have that bit set.
+
+Then, if there are n integers in the array and k of them have a particular bit set and (n-k) do not, 
+then that bit contributes nx(n-k) to the hamming distance. (Because for each of the 'k' numbers, we 
+have 1 of the (n-k) numbers, that have a different bit and the number of ways to select 1 from k 
+and (n-k) numbers is (kC1)x((n-k)C1)
+*/
+
 #include <iostream>
 #include <vector>
 
@@ -9,52 +19,22 @@ using namespace std;
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
-       vector<vector<int>> distancePairs;
-       vector<int> temp;
-       int ans=0;
-
-       pairCombinations_rec(distancePairs, 0, 1, nums, temp);
-
-       for(auto pair: distancePairs){
-           ans+=hammingDistance(pair[0],pair[1]);
-
-       } 
-
-       return ans;
-
-    }
-    
-
-
-
-    int hammingDistance(int x, int y) {
-        int hd=0;
-
-        for (int i=0; i<32; ++i){
-            if (((x>>i) & 1) != ((y>>i) & 1))            
-                hd++;
+        //from _geeky_coder_ on leetcode
+        int n=nums.size(), ans=0;
+        for(int i=0;i<32;i++){
+            int count=0; //Count of the no.of elements that have the 'i'th bit set
+            for(int k=0;k<n;k++){
+                count += (nums[k]>>i)&1; //if a number has this bit set it will return 1 if not it will return 0
+                //so this counts the total bits in teh array that are different.
+            }
+            ans += count*(n-count);  //i think this is the combination of bits getting folded into answer
         }
-        return hd; 
+        return ans;
+           
     }
-
-    void pairCombinations_rec(vector<vector<int>> &distancePairs, int idx1, int idx2, vector<int> &nums, vector<int> &temp){
-        if(idx1==nums.size()-1) return;
-        temp.push_back(nums[idx1]);
-        temp.push_back(nums[idx2]);
-        distancePairs.push_back(temp);
-        temp.clear();
-
-        if ( idx2 == nums.size()-1)
-            pairCombinations_rec(distancePairs, idx1+1, idx1+2,nums, temp);
-        if(idx2<nums.size()-1)
-            pairCombinations_rec(distancePairs, idx1, idx2+1, nums, temp);
-
-        return;
-
-    }
-
-
 };
+
+
 
 int main(){
     Solution sol;
