e23d0bdd57dfd2fe59168e504bf7bd9195a13080
cavanj-pats <cavanj@protonmail.com>
2021-12-12 17:18:40 -0500

    cpluspluslabs.h

---
+++ cpluspluslabs.h
@@ -0,0 +1,23 @@
+//cpluspluslabs.h
+#include <string>
+
+using namespace std;
+
+
+//void merge(int arr[], int, int, int);
+
+
+/*
+*  Week 31
+Add Node Tail Function.  
+used by addnodet.cpp 
+
+*/
+class Node
+{
+  public:
+  int key;
+  Node *left;
+  Node *right;
+  int height;
+}; 
