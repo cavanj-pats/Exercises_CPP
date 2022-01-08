18646eb14f40a460e508f8b2213dd7a14806dae9
cavanj-pats <cavanj@gmail.com>
2022-01-07 21:47:08 -0500

    leet 101 tweaks

--- leet 253 BinaryTreeLowestCommonAncestor.cpp
+++ leet 253 BinaryTreeLowestCommonAncestor.cpp
@@ -1,11 +1,12 @@
-//leet 104. Max Depth Binary Tree 
-//leet 104MaxDepth BinaryTree.cpp
-
+//leet 112 Binary Tree Path SUm
+// leet 112 Binary Tree Path Sum.cpp
 
 
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <map>
+#include <algorithm>>
 
 //for math library some people in discussion are saying
 //the define, and two includes
@@ -31,7 +32,130 @@ struct TreeNode {
 
 class Solution {
 public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        TreeNode* temp = root;
+        vector<int> pPath;
+        vector<int> qPath;
+
+        if (!root) return root;
+
+        
+
+    }
+//};
+     
+     
+     
+     
+     
+     
+     
+     bool findTarget(TreeNode* root, int k) {
+         vector<int> values;
+         vector<int> &v = values;
+         vector<int> ans;
+
+         inOrderVectorFill(root, v);
+         //sort(v.begin(), v.end());
+
+        ans = twoSum(v, k);
+
+        if (ans.size() != 0)  return true;
+
+        return false;
+    }
+     
+     
+     
+    // in order push tree values onto vector then use old TwoSum function.
+    void inOrderVectorFill(TreeNode* root, vector<int> &vValues ) {
+        if (root != nullptr){
+            vValues.push_back(root->val);
+            inOrderVectorFill(root->left, vValues );
+            inOrderVectorFill(root->right, vValues);            
+        }
+    }
+
+     vector<int> twoSum(vector<int>& nums, int target) {       
+        vector<int> ans;
+        bool solutionFound = false;
+               
+        ans.clear();
+        sort(nums.begin(), nums.end());
+        int x = 0;
+        int y=nums.size()-1;
+       
+        while (x < y){
+            if(nums[x] + nums[y] <  target){
+                ++x;
+            }
+            else if (nums[x]+ nums[y] >target){
+                --y;
+            } else{
+                ans.push_back(nums[x]);
+                ans.push_back(nums[y]);
+                solutionFound = true;
+                while (x < y && (nums[x] == nums[x+1])) ++x;
+                while (x < y && (nums[y] == nums[y-1])) --y;
+                ++x; --y;
+            }        
+                
+        }
+        return ans;
+    }
+
+
+    bool hasPathSum(TreeNode* root, int targetSum) {
+
+         map<bool, int> response;
+         //map<bool, int> &response = r;
+         response.insert(response.begin(), std::pair<bool, int> (false,0)  );
+         response.insert(response.begin(), std::pair<bool, int>(true, 0) );
+        
+        inorderSum (root, 0, targetSum, response);
+
+        return response[true] > 0;
+
+    }
+
+
     
+    
+    void inorderSum(TreeNode* root, int runningSum, int target, map<bool, int> &response) {
+        if (root != nullptr){
+            runningSum += root->val;
+            inorderSum(root->left, runningSum, target, response);
+                   
+            inorderSum(root->right, runningSum, target, response);
+            
+        } else{
+            if (runningSum == target) 
+                response[true]++;
+        }
+
+        response[false]++;
+    }
+    
+    // ****************
+     TreeNode* invertTree(TreeNode* root) {
+        
+        invertHelper(root);
+
+        return root;
+    }
+
+    void invertHelper(TreeNode* root){
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+
+        if (root->left) invertHelper(root->left);
+        if (root->right) invertHelper(root->right);
+
+        return;
+    }
+    //****************************************************8
     bool isSymmetric(TreeNode* root) {
         //I believe if I determine the height and node count on each subtree and if they are equal return TRUE    
         
@@ -43,13 +167,10 @@ public:
 
     bool symmetricHelper(TreeNode* left,  TreeNode* right){
         if (!left && !right){
-           
             return true;
         }
         else{
-            
             if((!left && right) || (left && !right)) return false;  //unbalanced tree
-            if (left->val != right->val) return false; 
             if( symmetricHelper (left->left, right->right) && symmetricHelper(left->right, right->left)) return true;
         }
         
@@ -129,25 +250,31 @@ public:
 
         return result;
     }
-};
 
+
+};
 
 
 
 
 int main()
 {
-	TreeNode node(1);
-    node.left = new TreeNode (2);
-    node.right = new TreeNode (3);
-    //node.left->right = new TreeNode (11);
-    //node.right->left = new TreeNode (15);
-    //node.right->right = new TreeNode (3);
+	TreeNode node(3);
+    node.left = new TreeNode (9);
+    node.right = new TreeNode (20);
+    node.left->right = new TreeNode (11);
+    node.right->left = new TreeNode (15);
+    node.right->right = new TreeNode (7);
 
     TreeNode* root = &node;
     Solution sol;
+    
+    cout << "Path Sum = 30: "<< sol.hasPathSum(root, 30 )<< endl<<endl;
+    
     vector<vector<int> > ans = sol.levelOrder(root);
     
+    TreeNode* swapped = sol.invertTree(root);
+
     for(vector<int> t : ans){
          cout << "[";
         for(int n=0; n<t.size(); ++n){
@@ -165,7 +292,7 @@ int main()
     }
 
     cout << "height: " << sol.maxDepth(root) << endl;
-    cout << "Symmetric Tree: " << (bool) sol.isSymmetric(root) << endl;
+    cout << "Symmetric Tree: " <<  sol.isSymmetric(root) << endl;
 
 	
 	return 0;
