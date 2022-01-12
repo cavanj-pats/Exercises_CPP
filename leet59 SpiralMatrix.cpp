a63832ff08b1d6171b91fad3016f684741a4453d
cavanj-pats <cavanj@gmail.com>
2022-01-12 14:32:29 -0500

    leet 59 Spiral Matrix

---
+++ leet59 SpiralMatrix.cpp
@@ -0,0 +1,33 @@
+//leet 59 Spiral Matrix II
+//1/12/2022
+
+/*
+Fill the matrix n x n in a spiral manner
+1. across top row.
+2. down right most column
+3. right to left across bottom row
+4. up first column but do not over -write 0,0
+5. go across 0+1 row
+6. down n-1 column
+7. across n-1 row  right to left
+8. stop at 0+1 row
+repeat
+odd size matrix will end in center.
+*/
+
+
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        int offeset = 0;
+
+        for (int i = offset; i< n-offset; ++i){
+            //fill row
+            if (i == n-offset -1){
+                //down column
+            }
+            
+        }
+        
+    }
+};
