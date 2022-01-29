69dbab196d101ded862a7f487cc6e354430f56da
cavanj-pats <cavanj@protonmail.com>
2022-01-29 17:25:43 -0500

    leet 707 acccepted by leet

--- leet 707 Design Linked List.cpp
+++ leet 707 Design Linked List.cpp
@@ -2,6 +2,11 @@
 
 #include <iostream>
 
+/*
+Runtime: 43 ms, faster than 60.92% of C++ online submissions for Design Linked List.
+Memory Usage: 19.7 MB, less than 29.11% of C++ online submissions for Design Linked List.
+*/
+
 
 //* Definition for singly-linked list.
   struct ListNode {
@@ -36,60 +41,77 @@ public:
         }
         else{
             ListNode* t = new ListNode(val);
-            t->next = head;
+           t->next = head;
             head = t;
+
+           // delete t;
         }
         ++size;
+        
         
     }
     
     void addAtTail(int val) {
-        ListNode* t = new ListNode(val);
-        ListNode* temp = head;
-        while (temp->next){
-            temp = temp->next;
+       if (size == 0){
+            addAtHead(val);
+        }else{
+            ListNode* t = new ListNode(val);
+            ListNode* temp = head;
+            while (temp->next){
+                temp = temp->next;
+            }
+            temp->next = t;
+             ++size;
         }
-        temp->next = t;
-        ++size;
         
     }
     
     void addAtIndex(int index, int val) {
-        if (index > size-1) return;
-        ListNode* t = new ListNode(val);
-        ListNode* temp = head;
-        ListNode* previous = head;
-        
-        for(int i=0; i< index; ++i){
-            previous = temp;
-            temp = temp->next;
+        if (index > size) return;
+        if (index == 0){
+            addAtHead(val);
+        } else if(index == size){
+            addAtTail(val);
+        } else{
+            ListNode* t = new ListNode(val);
+            ListNode* temp = head;
+            ListNode* previous = head;
             
-        }
+            for(int i=0; i< index; ++i){
+                previous = temp;
+                temp = temp->next;
+                
+            }
+            
+            t->next = temp;  //new node points to insertion point
         
-        t->next = temp;  //new node points to insertion point
-       
-        if (index ==0 ) {
-            head = t;
-        }
-        else{
-            previous->next = t;  //temp become t but previous node is not pointing to t
-        }
+            if (index ==0 ) {
+                head = t;
+            }
+            else{
+                previous->next = t;  //temp become t but previous node is not pointing to t
+            }
         
-        ++size;
+            ++size;
+        }
     }
     
     void deleteAtIndex(int index) {
         ListNode* t = head;
         ListNode* temp = head;
         if (index <0) return;
-        if (index > size-1) return;
-        for (int i = 0; i< index; ++i){
-            temp=t;
-            t = t->next;
+        if (index >= size) return;
+        if (index == 0){
+            head = temp->next;
+        }else{
+            for (int i = 0; i< index; ++i){
+                temp=t;
+                t = t->next;
             
+            }
+            
+            temp->next = t->next;
         }
-        
-        temp->next = t->next;
         --size;
     }
 
@@ -117,8 +139,9 @@ int main(){
     obj->deleteAtIndex(2);
     obj->addAtHead(6);
     obj->addAtTail(4);
+    obj->addAtIndex(0,99);
     std::cout << obj->get(0) << std::endl;
-    
+     std::cout << obj->get(4) << std::endl;
    // obj->addAtIndex(1,2);
     
 
@@ -128,9 +151,9 @@ int main(){
     }
     std::cout << std::endl;
 
-    obj->deleteAtIndex(1);
+    obj->deleteAtIndex(5);
 
-    std::cout << "Size after delete: " << obj->size << std::endl;
+    std::cout << "Size after delete item (5): " << obj->size << std::endl;
     for(int i = 0; i<obj->size; ++i){
         std::cout << obj->get(i) << ", ";
     }
