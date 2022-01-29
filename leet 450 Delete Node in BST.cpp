41a15e5524a3bfbaa76791f3cd9f41e3180e1ba9
Jim Cavanaugh <cavanj@protonmail.com>
2022-01-28 19:11:55 -0500

    leet 450 accepted

--- leet 450 Delete Node in BST.cpp
+++ leet 450 Delete Node in BST.cpp
@@ -1,5 +1,7 @@
 //leet 450 Delete Node in BST
 
+//i gave up and used leetcode discussion code
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -14,51 +16,36 @@
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        TreeNode* temp = root;
-        TreeNode* prev = root;
-        
-        if (root == NULL) return NULL;
-        
-        if(key != temp->val){
-            if (key < temp->val) {
-                nodeDeleteHelper(temp->left, prev, key);
-            }else
-            { nodeDeleteHelper(temp->right, prev, key);}
-        } else if (!temp->left && !temp->right){
-            //we found the one node it it must be deleted
-            return NULL;
-        } else if (temp->right){
-            temp->right->left = temp->left;
-            return temp->right;
-             
-        }else {
-            return temp->left;
+        TreeNode* iter = root, *par = nullptr;
+		// search for key node & keep a pointer to current node's parent
+        while(iter && iter -> val != key) {                              
+            par = iter;
+            if(iter -> val < key) iter = iter -> right;
+            else iter = iter -> left;
         }
+        if(!iter) return root;                                           // node not found  => Case:1
+        // iter is the node to be deleted
         
+        // node found with less than two children  => Case-2/3/4 combined
+        if(!iter -> left or !iter -> right) {
+            auto child = iter -> left ? iter -> left : iter -> right;    // find child node of iter if it exists
+            if(!par) root = child;                                       // iter is root node. Update root as child of iter
+            else if(par -> left == iter) par -> left = child;            // iter is left child. Update its parent's left pointer as iter's child
+            else par -> right = child;                                   // Else update parent's right pointer as iter's child
+        }
+        // node found with both children => Case-5
+        else {
+            auto cur = iter;                                              // cur maintains a reference to the node to be deleted
+            par = iter, iter = iter -> right;                             // go to right subtree 
+            while(iter -> left) par = iter, iter = iter -> left;          // and find smallest node in that right subtree
+            cur -> val = iter -> val;                                     // delete by replacing with smallest node found
+			// smallest node replaced from right subtree may have a right child. 
+			// So update that node's parent to hold the right child
+            if(par -> left == iter) par -> left = iter -> right;          
+            else par -> right = iter -> right;
+        }
+		// dont show the interviewer that you are a leaker :)
+        delete iter;        // free the memory		
         return root;
-    }
-        
-    
-    void nodeDeleteHelper(TreeNode* node, TreeNode* prev, int key){
-        
-        if (key == node->val){
-            //delte the key
-            if (node->right){
-                node->right->left = node->left;
-                prev->left = node->right;
-            }else if(node->left){
-                prev->left = node->left;
-            }else if (prev->left->val == key){
-                prev->left = nullptr;
-            }else prev->right = nullptr;
-            
-        } else if (key < node->val){
-            prev = node;
-            if (node->left) nodeDeleteHelper(node->left, prev, key);
-        }
-        else{
-            prev = node;
-            if(node->right) nodeDeleteHelper(node->right, prev, key);
-        }
     }
 };
