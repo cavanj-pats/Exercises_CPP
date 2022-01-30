fc2b563b96535f83d897922875c952d599b97c88
cavanj-pats <cavanj@protonmail.com>
2022-01-30 13:58:45 -0500

    leet 347 Submitted and Accepted

--- leet 347 TopKMostFrequentElements.cpp
+++ leet 347 TopKMostFrequentElements.cpp
@@ -1,13 +1,29 @@
 //leet 347 Top K most frequent elements
 
+
+/*
+Success
+Details 
+Runtime: 16 ms, faster than 77.86% of C++ online submissions for Top K Frequent Elements.
+Memory Usage: 13.6 MB, less than 71.77% of C++ online submissions for Top K Frequent Elements.
+*/
+
 #include <unordered_map>
 #include <iostream>
 #include <vector>
-#include <set>
 #include <algorithm>
 
 using namespace std;
 
+//this is from stackoverflow but is an idea I had so I am ok with using this code
+//i changed name from less_second to gt_secomd because I changed operator to greater than
+template <typename T1, typename T2>
+struct gt_second {
+    typedef pair<T1, T2> type;
+    bool operator ()(type const& a, type const& b) const {
+        return a.second > b.second;
+    }
+};
 
 class Solution {
 public:
@@ -20,27 +36,27 @@ public:
        // unordered_map<int, int>:: iterator it;
         
         unordered_map<int,int> intMap;
-        unordered_map<int,int>::iterator it;
-
+      
        vector<int> result;
-        for (int i=0; i<nums.size(); ++i){
+        int i = 0;
+        int j = nums.size()-1;
+        bool even = nums.size()%2 ==0 ? true : false ;
+        while (i<j){
             intMap[nums[i]]++;
+            intMap[nums[j]]++;
+            ++i;
+            --j;
         }
+        if (!even) intMap[nums[i]]++;
 
-        sort(intMap.begin(), intMap.end(), cmp);
-
+        vector<pair<int, int> > mapcopy(intMap.begin(), intMap.end());
+        sort(mapcopy.begin(), mapcopy.end(), gt_second<int, int>());
         int count = 0;
-        for(it = intMap.end(); it==intMap.begin();){
-            //it = intMap.end();
-         //   --it;
-            intMap.erase(it);
-            result.push_back(it->first);
-            if (count++ == k) break;
-            
+        for (auto m : mapcopy){
+            result.push_back(m.first);
+            if(++count == k) break;
         }
-        
-        
-        
+      
 
     return result;
         
@@ -63,6 +79,12 @@ int main(){
     Solution sol;
     vector<int> nums = {2,2,2,4,5,6,6};
     vector<int> result = sol.topKFrequent(nums, 2);
+
+    for(int i = 0; i<result.size(); ++i)
+        cout <<  result[i] <<", ";
+
+
+    cout << endl;   
 
     return 0;
 }
