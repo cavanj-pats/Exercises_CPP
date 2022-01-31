ce6513fd11ba6c3f4b3675bcb8516480d471e1ba
cavanj-pats <cavanj@protonmail.com>
2022-01-30 19:50:40 -0500

    leet 173 and leet 560

--- leet 173 BinarySearchTreeIterator.cpp
+++ leet 173 BinarySearchTreeIterator.cpp
@@ -18,6 +18,57 @@ You may assume that next() calls will always be valid. That is, there will be at
  in the in-order traversal when next() is called.
 */
 
+/*
+// leet 173 Binary Search Tree Iterator
+
+/*
+https://leetcode.com/problems/binary-search-tree-iterator/discuss/1430547/C%2B%2B-Simple-Solution-using-Stack-O(h)-Time-Complexity-(-with-Diagrammatic-Explanation-)
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/*
+class BSTIterator {
+public:
+    stack<TreeNode*> s;
+    BSTIterator(TreeNode* root) {
+        partialInorder(root);
+    }
+    
+    void partialInorder(TreeNode* root){
+        while(root != NULL){
+            s.push(root);
+            root = root->left;
+        }
+    }
+    
+    int next() {
+        TreeNode* top = s.top();
+        s.pop();
+        partialInorder(top->right);
+        return top->val;
+    }
+    
+    bool hasNext() {
+        return !s.empty();
+    }
+};
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
 
 
 /**
