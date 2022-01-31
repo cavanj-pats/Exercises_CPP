5628a30b3faddd2fbbbaf5ae91e3017d891c3fc6
cavanj-pats <cavanj@protonmail.com>
2022-01-30 19:52:44 -0500

    leet 560

--- leet 560 SubarraySumEqualsK.cpp
+++ leet 560 SubarraySumEqualsK.cpp
@@ -2,6 +2,11 @@
 
 //leet 560 Subarray
 
+/*
+Given an array of integers nums and an integer k, return the total number of continuous subarrays whose 
+sum equals to k.
+*/
+
 #include <iostream>
 #include <vector>
 #include <map>
