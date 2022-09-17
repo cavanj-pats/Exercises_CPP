0e1e8833aabc93a22ed482e9852879349ea86781
Jim Cavanaugh <cavanj@protonmail.com>
2022-09-17 19:14:02 -0400

    buld errors Class Arithmetic

---
+++ AbdulClassArithmetic.cpp
@@ -0,0 +1,46 @@
+//
+//
+#include <iostream>
+
+using namespace std;
+
+class Arithmetic{
+    private:
+        int a;
+        int b;
+    public:
+        Arithmetic(int a, int b);
+        int add();
+        int sub();
+        
+};
+
+Arithmetic::Arithmetic(int a, int b){
+    this->a = a;
+    this->b = b;
+}
+
+int Arithmetic::add(){
+    int c;
+    c = a+b;
+    return c;
+}
+
+int Arithmetic::sub(){
+    int c;
+    c = a-b;
+    return c;
+}
+
+
+
+int maint(){
+    Arithmetic ar(9,7);
+
+    cout << "Add: " << ar.add() << endl;
+    cout << "Sub: " << ar.sub() << endl;
+
+
+    return 0;
+
+}
