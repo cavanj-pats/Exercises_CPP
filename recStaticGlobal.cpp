44c0afb90597facdd565a310642ed877b4b91041
Jim Cavanaugh <cavanj@protonmail.com>
2022-10-03 20:11:45 -0400

    final static global

--- recStaticGlobal.cpp
+++ recStaticGlobal.cpp
@@ -6,6 +6,28 @@
 
 using namespace std;
 
+int y = 0;
+
+int fun(int n){
+    static int x = 0;
+    if (n>0){
+        x++;
+        return fun(n-1) + x;
+    }
+    return 0;
+}
+
+int fung(int n){
+    if (n>0){
+        y++;
+        return fung(n-1)+y;
+
+    }
+
+    return 0;
+}
+
+//fun1 is the base function
 int fun1(int n){
 
     if (n>0){
@@ -23,6 +45,13 @@ int main(){
 
     printf("fun1(%d)= %d", x1, fun1(x1));
     cout << endl;
+
+    printf ("Static run #1 %d\n", fun(x1));
+    printf ("Static run #2 %d\n", fun(x1));
+    cout << endl;
+    printf ("Global run #1 x1: %d = %d\n", x1,fung(x1));
+    printf ("Global run #2 x1: %d = %d\n", x1,fung(x1));
+
 
     return 0;
 }
