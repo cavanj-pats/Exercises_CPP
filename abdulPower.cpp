a93bfc04bcc48bc782b4a3551228d25a734d18d5
Jim Cavanaugh <cavanj@protonmail.com>
2022-10-04 21:01:38 -0400

    power with fastpow

--- abdulPower.cpp
+++ abdulPower.cpp
@@ -11,11 +11,23 @@ int pow(int m, int n){
         return m * pow(m,n-1);
 }
 
+int fastpow(int m, int n){
+    if (n==0)
+        return 1;
+    
+    if (n%2 == 0)
+        return fastpow(m*m, n/2);
+    else  
+        return m*fastpow(m*m, n/2);  
+
+}
+
 int main(){
     int m = 2;
-    int n = 4;
+    int n = 5;
 
     printf("%d \n", pow(m,n));
+    printf("%d \n", fastpow(m,n));
 
     return 0;
 }
