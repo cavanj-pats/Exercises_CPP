fd02fb98e33c0a7e4fa6e6306c5335fd46a56810
Jim Cavanaugh <cavanj@protonmail.com>
2023-03-30 21:47:37 -0400

    abdul Array ADT search

--- abdulArrayADTstruct.cpp
+++ abdulArrayADTstruct.cpp
@@ -33,12 +33,13 @@ void arrAppend(Array *arr, int x)
 
 void arrInsert(Array *arr, int idx, int x)
 {
+    //probably should check that arr->length < arr->size;
+
     if(idx > arr->length) return;
 
     for(int i = arr->length; i>idx; i--)
-    {
         arr->A[i] = arr->A[i-1];
-    }
+    
     arr->A[idx] = x;
     arr->length++;
 
@@ -49,12 +50,34 @@ void arrDelete(Array *arr, int idx)
 {
     //remove item at index idx
 
-    for(int i = idx; i<arr->length-1; i++)
+    //abduls function returned the value at that index
+    //probably a good idea to check that you deleted the correct element
+    //not sure how to undo it.....
+    if(idx >=0 && idx < arr->length)
     {
-        arr->A[i]= arr->A[i+1];
+        for(int i = idx; i<arr->length-1; i++)
+            arr->A[i]= arr->A[i+1];
+        
+    
+        arr->length--;
     }
-    arr->length--;
     return;
+}
+
+int arrSearch(Array *arr, int value)
+{
+    int j = 0;
+    int k = arr->length-1;
+
+    while (j<=k)
+    {
+        if(arr->A[j] == value) return j;
+        if(arr->A[k] == value) return k;
+        j++;
+        k--;
+
+    }
+    return -1;
 }
 
 
@@ -68,8 +91,6 @@ int main()
     arr.A[1] = 4;
     arr.A[2] = 6;
     arr.A[3] = 8;
-
-
     arr.length = 4;
     
     arrDisplay(arr);
@@ -79,6 +100,10 @@ int main()
     arrDisplay(arr);
     arrDelete(&arr,0);
     arrDisplay(arr);
+    if (!(arrSearch(&arr,10) == -1))
+        printf("found\n");
+    else
+        printf("Not found\n");
 
     free(arr.A);
 
