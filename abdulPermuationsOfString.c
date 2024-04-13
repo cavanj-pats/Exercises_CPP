11e2962efae4ea7c57c04282aa691679c55a17a6
Jim Cavanaugh <cavanj@protonmail.com>
2024-04-12 21:51:11 -0400

    permutations two ways

--- abdulPermuationsOfString.c
+++ abdulPermuationsOfString.c
@@ -3,6 +3,14 @@
 
 #include "stdio.h"
 
+
+void cswap(char *s, int i, int l)
+{
+    char temp = s[i];
+    s[i]=s[l];
+    s[l]=temp;
+
+}
 void perm(char *s, int k)
 {
     static int A[10]={0};
@@ -18,24 +26,45 @@ void perm(char *s, int k)
     else{
         for(i = 0; s[i]!='\0'; i++)
         {
-            if (A[i]==0)
+            if (A[i]==0)    //if it is zero then it has not been added to the res solution
             {
-                res[k] = s[i];
-                A[i]=1;
-                perm(s, k+1);
-                A[i]=0;
+                res[k] = s[i];     //add the character to res
+                A[i]=1;            //set the A flag
+                perm(s, k+1);      //call to check for another permutation
+                A[i]=0;             //if perm returns then all items were found so turn off flags
             }
         }
     }
 
 }
 
+void permS(char *s, int l, int h)
+{
+   int i;
+
+   if (l==h)
+    {
+        printf("%s\n",s);
+
+    }
+    else{
+        for(i=l; i<=h;i++)
+        {
+            cswap(s, l, i);
+            permS(s, l+1, h);
+            cswap(s, l, i);
+
+        }
+    }
+}
 
 int main()
 {
-    char s[]={'A', 'B','C'};
+    char s[]={'A', 'B', 'C', 'D'};
 
     perm(s,0);
+    printf("\n");
+    permS(s,0,3 );
 
     return 0;
 }
