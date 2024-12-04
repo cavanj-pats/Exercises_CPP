07cdcb6fb05820fc097d25d493412d4f83dd23e6
Jim Cavanaugh <cavanj@protonmail.com>
2024-12-04 17:29:28 -0500

    Fortran Play

---
+++ hello.f90
@@ -0,0 +1,14 @@
+program hello
+    ! This is a comment line; it is ignored by the compiler
+    implicit none
+    real::pi = 3.1415927
+
+    print *, pi
+
+    print *, 'Hello, World!'
+
+
+
+
+
+  end program hello
