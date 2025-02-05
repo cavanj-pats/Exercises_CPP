ac867dc8e66de695babcc08550fd235799c701c5
Jim Cavanaugh <cavanj@protonmail.com>
2025-02-04 20:37:42 -0500

    Stack Array complete not fully tested

--- abdulStackArray.c
+++ abdulStackArray.c
@@ -12,23 +12,54 @@ struct Stack
     int * s;
 };
 
+void createStack(struct Stack *st, int size)
+{
+    st->s = (int *) malloc(size * sizeof(int));
+    st->top = -1;
+    st->size = size;
+}
+
 void push(struct Stack * st, int x)
 {
-    st->s[++st->top] = x;
+    if(st->top < st->size-2)
+        st->s[++st->top] = x;
       //top is number of elements. top element on largest index
 }
 
-int peek(struct Stack * st)
+int peek(struct Stack st,  int pos)
 {
-    return st->s[st->top];  
+    //abdul's peek takes a stack by value and a position
+    //he derived an expression to determine array index.
+    //it is ::  [top - position + 1]
 
+    int x = -1;
+
+    //verify pos is valid.
+    if(st.top - pos + 1 < 0)
+    {
+        printf("Invalid Position!\n");
+    }
+    else{
+        x =  st.s[st.top - pos + 1];  
+    }
+
+    return x;
 }
 
 int pop(struct Stack * st)   //delete the top value
 {
-    int val = st->s[st->top];
-    st->top--;
-    return val;
+    int x = -1;
+    if(st->top == -1)
+    {
+        printf("Stack is empty return -1\n");
+    }
+    else 
+    {
+        x = st->s[st->top];
+        st->top--;
+    }
+
+    return x;
 }
 
 void printStack(struct Stack * st)
@@ -38,17 +69,32 @@ void printStack(struct Stack * st)
 
 }
 
+int isFull(struct Stack st)
+{
+    if(st.top == st.size- 1)
+        return 1;
+
+    return 0;
+}
+
+int isEmpty(struct Stack st)
+{
+    if(st.top == -1) return 1;
+
+    return 0;
+}
+
 
 int main()
 {
     struct Stack * st;
-
+    int sz;
 
 
     printf("Please enter size of Stack: ");
-    scanf("%d", &st->size);
-    st->top = -1;   //set initial size of Stack
-    st->s =  (int *) malloc(st->size * sizeof(int));
+    scanf("%d", &sz);
+    createStack(st, sz);
+    
     push(st,4);
     push(st,9);
     
