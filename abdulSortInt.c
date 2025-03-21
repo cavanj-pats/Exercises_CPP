f3897e79eb751cec0d84c8342d128c5d75df482c
Jim Cavanaugh <cavanj@protonmail.com>
2025-03-20 22:35:19 -0400

    SORT up to itMergeSort

--- abdulSortInt.c
+++ abdulSortInt.c
@@ -54,6 +54,51 @@ void insertionSort(int A[], int n)
     }
 }
 
+void merge(int A[], int l, int mid, int h)
+{
+    int i = l;
+    int j = mid+1;
+    int k = l;
+    int B[100];   //used B[h+1] did not work can also use B[100] which is plenty big enough.   B is the auxiliary array
+    while(i<=mid && j <=h)
+    {
+        if(A[i] < A[j])
+            B[k++] = A[i++];
+        else
+            B[k++] = A[j++];
+    }
+    for(; i<=mid; i++)
+        B[k++] = A[i];
+    for(; j<=h; j++)
+        B[k++] = A[j];
+
+    //copy B contents back to A
+    for(i = l; i<=h; i++)
+        A[i] = B[i];
+
+}
+
+void itMergeSort(int A[], int n)
+{
+    int p, l, h, mid ;
+    //p is for passes
+
+    for(p=2; p<=n; p=p*2)
+    {
+        for(int i=0; i+p-1<n; i=i+p)
+        {
+            l=i;
+            h=i+p-1;
+            mid=(l+h)/2;
+            merge(A,l,mid,h);
+
+        }
+    }
+    if(p/2<n)
+        merge(A,0,p/2-1,n);
+}
+
+
 void display(int A[], int n)
 {
     for(int i = 0; i<n; i++)
@@ -65,11 +110,12 @@ void display(int A[], int n)
 
 int main()
 {
-    int A[]={8, 8, 7, 3, 2};
+    int A[]={8, 3, 7, 2, 9};
     int n = 5;
 
     display(A,n);
-    bubbleSort(A, n);
+    //bubbleSort(A, n);
+    itMergeSort(A, n);
     display(A,n);
 
     return 0;
