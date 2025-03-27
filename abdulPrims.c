6a6460c94815660e21c5be0bbac37ec9b1dacbe5
Jim Cavanaugh <cavanj@protonmail.com>
2025-03-26 20:34:14 -0400

    M S Graph Prims working

--- abdulPrims.c
+++ abdulPrims.c
@@ -21,14 +21,14 @@ int cost[8][8] =
 
 int near[8]={ I,I,I,I,I,I,I,I};
 
-int t[2][7];
+int t[2][6];
 
 int main()
 {
-    int i, j, k, u, v, n=8, min=I;
-    for(i=1; i<=n; i++)
+    int i, j, k, u, v, n=7, min=I;
+    for(i=1; i<n; i++)
     {
-        for(j=i; j<=n; j++)
+        for(j=i; j<n; j++)
         {
             if(cost[i][j]<min)
             {
@@ -45,41 +45,45 @@ int main()
     //initialization complete
 
     for(i=1;i<=n;i++)
+    {
+        if(near[i] != 0)
         {
             if(cost[i][u]<cost[i][v])
                 near[i]=u;
             else   
                 near[i]=v;
         }
+    }
 
-        for(i=1;j<n-1;i++)
+
+    for(i=1;i<n-1;i++)
+    {
+        min = I;
+        for(j=1; j<=n;j++)
         {
-            min = I;
-            for(j=1; j<=n;j++)
+            if(near[j]!=0 && cost[j][near[j]]<min)
             {
-                if(near[j]!=0 && cost[j][near[j]]<min)
-                {
-                    k=j;
-                    min=cost[j][near[j]];
+                k=j;
+                min=cost[j][near[j]];
 
-                }
-            
             }
-            t[0][i]=k;
-            t[1][i]=near[k];
-            near[k]=0;
-
-            for(j=1;j<=n;j++)
-            {
-                if(near[j]!=0 && cost[j][k]<cost[j][near[j]])
-                    near[j]=k;
-            }
+        
         }
+        t[0][i]=k;
+        t[1][i]=near[k];
+        near[k]=0;
 
-        for(i=0;i<n-1;i++)
+        for(j=1;j<=n;j++)
         {
-            printf("( %d, %d)", t[0][i], t[1][i]);
+            if(near[j]!=0 && cost[j][k]<cost[j][near[j]])
+                near[j]=k;
         }
+    }
+
+    for(i=0;i<n-1;i++)
+    {
+        printf("(%d, %d)\n", t[0][i], t[1][i]);
+    }
 
     return 0;
 }
